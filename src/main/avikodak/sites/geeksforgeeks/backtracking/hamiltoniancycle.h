06d837dc61c6662e94634d1d1249319bfac560c5
avikodak <avikodak@gmail.com>
2015-01-12 10:00:58 +0530

    graphs

--- src/main/avikodak/sites/geeksforgeeks/backtracking/hamiltoniancycle.h
+++ src/main/avikodak/sites/geeksforgeeks/backtracking/hamiltoniancycle.h
@@ -1,11 +1,11 @@
 /****************************************************************************************************************************************************
- *  File Name   		: numberislands.h 
- *	File Location		: D:\algos\algos_v2\src\main\avikodak\sites\geeksforgeeks\graph\page04\numberislands.h
- *  Created on			: Jan 11, 2015 :: 10:05:06 PM
+ *  File Name   		: hamiltoniancycle.h 
+ *	File Location		: D:\algos\algos_v2\src\main\avikodak\sites\geeksforgeeks\backtracking\hamiltoniancycle.h
+ *  Created on			: Jan 12, 2015 :: 9:59:32 AM
  *  Author				: AVINASH
  *  Testing Status 		: TODO
  *  URL 				: TODO
- ****************************************************************************************************************************************************/
+****************************************************************************************************************************************************/
 
 /****************************************************************************************************************************************************/
 /* 														NAMESPACE DECLARATION AND IMPORTS 														    */
@@ -67,51 +67,12 @@ using namespace __gnu_cxx;
 /* 																MAIN CODE START 																    */
 /****************************************************************************************************************************************************/
 
-#ifndef NUMBERISLANDS_H_
-#define NUMBERISLANDS_H_
+#ifndef HAMILTONIANCYCLE_H_
+#define HAMILTONIANCYCLE_H_
 
-bool icIsSafe(vector<vector<bool> > userInput,vector<vector<bool> > isVisited,int rows,int columns){
-	if(rows < 0 || columns < 0 || rows >= userInput.size() || columns >= userInput.size() || !userInput[rows][columns] || isVisited[rows][columns]){
-		return false;
-	}
-	return true;
-}
 
-void dfsArrayMain(vector<vector<bool> > userInput,vector<vector<bool> > &isVisited,int rows,int columns){
-	if(rows < 0 || columns < 0 || rows >= userInput.size() || columns >= userInput.size()){
-		return;
-	}
-	int yValues[] = {-1,-1,-1,0,0,1,1,1};
-	int xValues[] = {-1,0,1,1,1,0,-1,-1};
-	isVisited[rows][columns] = true;
-	for(unsigned int counter = 0;counter < 8;counter++){
-		if(userInput,isVisited,rows + yValues[counter],columns + xValues[counter]){
-			dfsArrayMain(userInput,isVisited,rows + yValues[counter],columns + xValues[counter]);
-		}
-	}
-}
 
-int numberOfIslands(vector<vector<bool> > userInput){
-	if(userInput.size() == 0 || userInput[0].size() == 0){
-		return 0;
-	}
-	vector<vector<bool> > visitedArray(userInput.size());
-	for(unsigned int rowCounter = 0;rowCounter  < userInput[0].size();rowCounter++){
-		visitedArray[rowCounter].assign(userInput[0].size(),false);
-	}
-	int islandCounter = 0;
-	for(unsigned int rowCounter = 0;rowCounter < userInput.size();rowCounter++){
-		for(unsigned int columnCounter = 0;columnCounter < userInput[0].size();columnCounter++){
-			if(userInput[rowCounter][columnCounter] && !visitedArray[rowCounter][columnCounter]){
-				islandCounter++;
-				dfsArrayMain(userInput,visitedArray,rowCounter,columnCounter);
-			}
-		}
-	}
-	return islandCounter;
-}
-
-#endif /* NUMBERISLANDS_H_ */
+#endif /* HAMILTONIANCYCLE_H_ */
 
 /****************************************************************************************************************************************************/
 /* 																MAIN CODE END 																	    */
--- src/main/avikodak/sites/geeksforgeeks/graph/page04/hamiltoniancycle.h
+++ src/main/avikodak/sites/geeksforgeeks/graph/page04/hamiltoniancycle.h
@@ -1,7 +1,7 @@
 /****************************************************************************************************************************************************
- *  File Name   		: numberislands.h 
- *	File Location		: D:\algos\algos_v2\src\main\avikodak\sites\geeksforgeeks\graph\page04\numberislands.h
- *  Created on			: Jan 11, 2015 :: 10:05:06 PM
+ *  File Name   		: hamiltoniancycle.h 
+ *	File Location		: D:\algos\algos_v2\src\main\avikodak\sites\geeksforgeeks\graph\page04\hamiltoniancycle.h
+ *  Created on			: Jan 12, 2015 :: 8:30:16 AM
  *  Author				: AVINASH
  *  Testing Status 		: TODO
  *  URL 				: TODO
@@ -67,51 +67,54 @@ using namespace __gnu_cxx;
 /* 																MAIN CODE START 																    */
 /****************************************************************************************************************************************************/
 
-#ifndef NUMBERISLANDS_H_
-#define NUMBERISLANDS_H_
+#ifndef HAMILTONIANCYCLE_H_
+#define HAMILTONIANCYCLE_H_
 
-bool icIsSafe(vector<vector<bool> > userInput,vector<vector<bool> > isVisited,int rows,int columns){
-	if(rows < 0 || columns < 0 || rows >= userInput.size() || columns >= userInput.size() || !userInput[rows][columns] || isVisited[rows][columns]){
+bool hcIsSafe(vector<vector<bool> > adjacencyMatrix,vector<int> cycle,int currentIndex,int vertex){
+	if(currentIndex >= cycle.size() || vertex >= adjacencyMatrix.size()){
 		return false;
+	}
+	if(!adjacencyMatrix[cycle[currentIndex-1]][vertex]){
+		return false;
+	}
+	for(unsigned int counter = 0;counter < currentIndex;counter++){
+		if(cycle[counter] == vertex){
+			return false;
+		}
 	}
 	return true;
 }
 
-void dfsArrayMain(vector<vector<bool> > userInput,vector<vector<bool> > &isVisited,int rows,int columns){
-	if(rows < 0 || columns < 0 || rows >= userInput.size() || columns >= userInput.size()){
-		return;
+bool hamiltonianCycleMain(vector<vector<bool> > adjacencyMatrix,vector<int> &cycle,int currentIndex){
+	if(adjacencyMatrix.size() == 0 || currentIndex > adjacencyMatrix.size()){
+		return false;
 	}
-	int yValues[] = {-1,-1,-1,0,0,1,1,1};
-	int xValues[] = {-1,0,1,1,1,0,-1,-1};
-	isVisited[rows][columns] = true;
-	for(unsigned int counter = 0;counter < 8;counter++){
-		if(userInput,isVisited,rows + yValues[counter],columns + xValues[counter]){
-			dfsArrayMain(userInput,isVisited,rows + yValues[counter],columns + xValues[counter]);
-		}
+	if(currentIndex == cycle.size()){
+		return adjacencyMatrix[currentIndex-1][0];
 	}
-}
-
-int numberOfIslands(vector<vector<bool> > userInput){
-	if(userInput.size() == 0 || userInput[0].size() == 0){
-		return 0;
-	}
-	vector<vector<bool> > visitedArray(userInput.size());
-	for(unsigned int rowCounter = 0;rowCounter  < userInput[0].size();rowCounter++){
-		visitedArray[rowCounter].assign(userInput[0].size(),false);
-	}
-	int islandCounter = 0;
-	for(unsigned int rowCounter = 0;rowCounter < userInput.size();rowCounter++){
-		for(unsigned int columnCounter = 0;columnCounter < userInput[0].size();columnCounter++){
-			if(userInput[rowCounter][columnCounter] && !visitedArray[rowCounter][columnCounter]){
-				islandCounter++;
-				dfsArrayMain(userInput,visitedArray,rowCounter,columnCounter);
+	for(unsigned int counter = 1;counter < adjacencyMatrix.size();counter++){
+		if(hcIsSafe(adjacencyMatrix,cycle,currentIndex,counter)){
+			cycle[currentIndex] = counter;
+			if(hamiltonianCycleMain(adjacencyMatrix,cycle,currentIndex+1)){
+				return true;
 			}
 		}
 	}
-	return islandCounter;
+	return false;
 }
 
-#endif /* NUMBERISLANDS_H_ */
+void printHamiltonianCycle(vector<vector<bool> > adjacencyMatrix){
+	if(adjacencyMatrix.size() == 0){
+		return;
+	}
+	vector<int> cycle(adjacencyMatrix.size(),INT_MIN);
+	cycle[0] = 0;
+	if(hamiltonianCycleMain(adjacencyMatrix,cycle,1)){
+		printIVector(cycle);
+	}
+}
+
+#endif /* HAMILTONIANCYCLE_H_ */
 
 /****************************************************************************************************************************************************/
 /* 																MAIN CODE END 																	    */
