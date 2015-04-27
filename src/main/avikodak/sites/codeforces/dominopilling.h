3040115c7a4acb8c60e6af8592da5347f37151c1
Avinash Kodakandla <avinashk@vaycayhero.com>
2015-04-27 23:59:55 +0530

    cff

---
+++ src/main/avikodak/sites/codeforces/dominopilling.h
@@ -0,0 +1,84 @@
+/****************************************************************************************************************************************************
+ *  File Name                   : dominopilling.h
+ *  File Location               : D:\projects\cpp\algos_v2\src\main\avikodak\sites\codeforces\dominopilling.h
+ *  Created on                  : Apr 27, 2015 :: 11:54:52 PM
+ *  Author                      : avikodak
+ *  Testing Status              : Tested
+ *  URL                         : http://codeforces.com/problemset/problem/50/A
+ ****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                         NAMESPACE DECLARATION AND IMPORTS                                                        */
+/****************************************************************************************************************************************************/
+
+using namespace std;
+using namespace __gnu_cxx;
+
+/****************************************************************************************************************************************************/
+/*                                                                 INCLUDES                                                                         */
+/****************************************************************************************************************************************************/
+
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <ctime>
+#include <list>
+#include <map>
+#include <set>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <hash_map>
+#include <stack>
+#include <queue>
+#include <limits.h>
+#include <algorithm/constants/constants.h>
+#include <algorithm/ds/commonds.h>
+#include <algorithm/ds/linkedlistds.h>
+#include <algorithm/ds/graphds.h>
+#include <algorithm/ds/mathds.h>
+#include <algorithm/ds/treeds.h>
+#include <algorithm/utils/arrayutil.h>
+#include <algorithm/utils/avltreeutil.h>
+#include <algorithm/utils/bplustreeutil.h>
+#include <algorithm/utils/btreeutil.h>
+#include <algorithm/utils/commonutil.h>
+#include <algorithm/utils/dillutil.h>
+#include <algorithm/utils/graphutil.h>
+#include <algorithm/utils/mathutil.h>
+#include <algorithm/utils/redblacktreeutil.h>
+#include <algorithm/utils/sillutil.h>
+#include <algorithm/utils/treeutil.h>
+#include <algorithm/utils/twofourtreeutil.h>
+
+/****************************************************************************************************************************************************/
+/*                                                            USER DEFINED CONSTANTS                                                                */
+/****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                             MAIN CODE START                                                                      */
+/****************************************************************************************************************************************************/
+
+#ifndef DOMINOPILLING_H_
+#define DOMINOPILLING_H_
+
+//Tested
+void getMaxNumberOfDominoes(){
+	unsigned int length,breadth;
+	scanf("%u %u",&length,&breadth);
+	printf("%u",(length*breadth)/2);
+}
+
+#endif /* DOMINOPILLING_H_ */
+
+/****************************************************************************************************************************************************/
+/*                                                               MAIN CODE END                                                                      */
+/****************************************************************************************************************************************************/
