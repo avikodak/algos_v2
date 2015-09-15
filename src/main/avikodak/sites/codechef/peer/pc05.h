87774a768576edd732a7b16326669acc901b114e
Avinash Kodakandla <avinashk@vaycayhero.com>
2015-09-15 10:47:54 +0530

    codechef

---
+++ src/main/avikodak/sites/codechef/peer/pc05.h
@@ -0,0 +1,105 @@
+/****************************************************************************************************************************************************
+ *  File Name                   : pc05.h
+ *  File Location               : /home/avikodak/Desktop/projects/algos_v2/src/main/avikodak/sites/codechef/peer/pc05.h
+ *  Created on                  : 15-Sep-2015 :: 10:35:18 am
+ *  Author                      : avikodak
+ *  Testing Status              : Tested
+ *  URL                         : https://www.codechef.com/problems/PC05/
+ ****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                         NAMESPACE DECLARATION AND IMPORTS                                                        */
+/****************************************************************************************************************************************************/
+
+using namespace std;
+using namespace __gnu_cxx;
+
+/****************************************************************************************************************************************************/
+/*                                                                 INCLUDES                                                                         */
+/****************************************************************************************************************************************************/
+
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <ctime>
+#include <list>
+#include <map>
+#include <set>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <hash_map>
+#include <stack>
+#include <queue>
+#include <limits.h>
+#include <stdint.h>
+#include <lib/constants/constants.h>
+#include <lib/ds/commonds.h>
+#include <lib/ds/linkedlistds.h>
+#include <lib/ds/graphds.h>
+#include <lib/ds/mathds.h>
+#include <lib/ds/treeds.h>
+#include <lib/utils/arrayutil.h>
+#include <lib/utils/avltreeutil.h>
+#include <lib/utils/bplustreeutil.h>
+#include <lib/utils/btreeutil.h>
+#include <lib/utils/commonutil.h>
+#include <lib/utils/dillutil.h>
+#include <lib/utils/graphutil.h>
+#include <lib/utils/mathutil.h>
+#include <lib/utils/redblacktreeutil.h>
+#include <lib/utils/sillutil.h>
+#include <lib/utils/treeutil.h>
+#include <lib/utils/twofourtreeutil.h>
+
+/****************************************************************************************************************************************************/
+/*                                                            USER DEFINED CONSTANTS                                                                */
+/****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                             MAIN CODE START                                                                      */
+/****************************************************************************************************************************************************/
+
+#ifndef PC05_H_
+#define PC05_H_
+
+//Tested
+unsigned int sumOfDigits(unsigned int userInput){
+	unsigned int sum = 0;
+	while(userInput){
+		sum += userInput%10;
+		userInput /= 10;
+	}
+	return sum;
+}
+
+//Tested
+void printResults(){
+	int primeNumbers[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31};
+	unsigned int testCases,lValue,hValue,total = 0;
+	scanf("%u",&testCases);
+	while(testCases--){
+		scanf("%u %u",&lValue,&hValue);
+		total = 0;
+		for(unsigned int counter = lValue;counter <= hValue;counter++){
+			if(binary_search(primeNumbers,primeNumbers+11,sumOfDigits(counter))){
+				total++;
+			}
+		}
+		cout << total << endl;
+	}
+}
+
+#endif /* PC05_H_ */
+
+/****************************************************************************************************************************************************/
+/*                                                               MAIN CODE END                                                                      */
+/****************************************************************************************************************************************************/
