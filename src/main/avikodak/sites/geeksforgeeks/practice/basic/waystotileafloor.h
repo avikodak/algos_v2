30d7c8901be455daf171f3b5377810c44c7084e6
Avinash Kodakandla <avikodak@gmail.com>
2016-03-16 20:33:04 +0530

    geeksforgeeks

---
+++ src/main/avikodak/sites/geeksforgeeks/practice/basic/waystotileafloor.h
@@ -0,0 +1,94 @@
+/****************************************************************************************************************************************************
+ *  File Name                   : waystotileafloor.h
+ *  File Location               : /algos_v2/src/main/avikodak/sites/geeksforgeeks/practice/basic/waystotileafloor.h
+ *  Created on                  : Aug 7, 2016 :: 12:37:05 PM
+ *  Author                      : avikodak
+ *  Testing Status              : Tested
+ *  URL                         : http://www.practice.geeksforgeeks.org/problem-page.php?pid=724
+ ****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                         NAMESPACE DECLARATION AND IMPORTS                                                        */
+/****************************************************************************************************************************************************/
+
+using namespace std;
+using namespace __gnu_cxx;
+
+/****************************************************************************************************************************************************/
+/*                                                                 INCLUDES                                                                         */
+/****************************************************************************************************************************************************/
+
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <ctime>
+#include <list>
+#include <map>
+#include <set>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <hash_map>
+#include <stack>
+#include <queue>
+#include <limits.h>
+#include <libv2/common/commonincludes.h>
+#include <libv2/constants/constants.h>
+#include <libv2/ds/commonds.h>
+#include <libv2/ds/graphds.h>
+#include <libv2/ds/linkedlistds.h>
+#include <libv2/ds/mathds.h>
+#include <libv2/ds/treeds.h>
+#include <libv2/utils/abtreeutil.h>
+#include <libv2/utils/arrayutil.h>
+#include <libv2/utils/avltreeutil.h>
+#include <libv2/utils/bplustreeutil.h>
+#include <libv2/utils/bstutil.h>
+#include <libv2/utils/btreeutil.h>
+#include <libv2/utils/commonutil.h>
+#include <libv2/utils/dillutil.h>
+#include <libv2/utils/graphutil.h>
+#include <libv2/utils/ioutil.h>
+#include <libv2/utils/mathutil.h>
+#include <libv2/utils/redblacktreeutil.h>
+#include <libv2/utils/sillutil.h>
+#include <libv2/utils/treeutil.h>
+#include <libv2/utils/trieutil.h>
+#include <libv2/utils/twofourtreeutil.h>
+
+/****************************************************************************************************************************************************/
+/*                                                            USER DEFINED CONSTANTS                                                                */
+/****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                             MAIN CODE START                                                                      */
+/****************************************************************************************************************************************************/
+
+#ifndef MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_BASIC_WAYSTOTILEAFLOOR_H_
+#define MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_BASIC_WAYSTOTILEAFLOOR_H_
+
+//Tested
+void solveProblem() {
+    unsigned int testCases;
+    long long int fibonacciNumbers[] = { 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418,
+            317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155, 165580141, 267914296, 433494437, 701408733,
+            1134903170, 1836311903, 2971215073, 4807526976, 7778742049, 12586269025, 20365011074, 32951280099, 53316291173, 86267571272, 139583862445, 225851433717, 365435296162,
+            591286729879, 956722026041, 1548008755920, 2504730781961, 4052739537881, 6557470319842, 10610209857723, 17167680177565, 27777890035288, 44945570212853, 72723460248141,
+            117669030460994, 190392490709135, 308061521170129 };
+    long long int input;
+    scanf("%u", &testCases);
+    while (testCases--) {
+        scanf("%lld", &input);
+        printf("%lld\n", fibonacciNumbers[input - 1]);
+    }
+}
+
+#endif /* MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_BASIC_WAYSTOTILEAFLOOR_H_ */
