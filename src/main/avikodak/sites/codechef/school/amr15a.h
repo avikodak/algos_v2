5509b3f5b7b4b6d63c5798b6d8e9a4a43c664ae3
Avinash Kodakandla <avikodak@gmail.com>
2016-04-17 22:27:30 +0530

    Codechef

---
+++ src/main/avikodak/sites/codechef/school/amr15a.h
@@ -0,0 +1,93 @@
+/****************************************************************************************************************************************************
+ *  File Name                   : amr15a.h
+ *  File Location               : /algos_v2/src/main/avikodak/sites/codechef/school/amr15a.h
+ *  Created on                  : Apr 17, 2016 :: 12:56:36 PM
+ *  Author                      : avikodak
+ *  Testing Status              : Tested
+ *  URL                         : https://www.codechef.com/problems/AMR15A
+ ****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                         NAMESPACE DECLARATION AND IMPORTS                                                        */
+/****************************************************************************************************************************************************/
+
+using namespace std;
+using namespace __gnu_cxx;
+
+/****************************************************************************************************************************************************/
+/*                                                                 INCLUDES                                                                         */
+/****************************************************************************************************************************************************/
+
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <ctime>
+#include <list>
+#include <map>
+#include <set>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <hash_map>
+#include <stack>
+#include <queue>
+#include <limits.h>
+#include <libv2/common/commonincludes.h>
+#include <libv2/constants/constants.h>
+#include <libv2/ds/commonds.h>
+#include <libv2/ds/graphds.h>
+#include <libv2/ds/linkedlistds.h>
+#include <libv2/ds/mathds.h>
+#include <libv2/ds/treeds.h>
+#include <libv2/utils/abtreeutil.h>
+#include <libv2/utils/arrayutil.h>
+#include <libv2/utils/avltreeutil.h>
+#include <libv2/utils/bplustreeutil.h>
+#include <libv2/utils/bstutil.h>
+#include <libv2/utils/btreeutil.h>
+#include <libv2/utils/commonutil.h>
+#include <libv2/utils/dillutil.h>
+#include <libv2/utils/graphutil.h>
+#include <libv2/utils/ioutil.h>
+#include <libv2/utils/mathutil.h>
+#include <libv2/utils/redblacktreeutil.h>
+#include <libv2/utils/sillutil.h>
+#include <libv2/utils/treeutil.h>
+#include <libv2/utils/trieutil.h>
+#include <libv2/utils/twofourtreeutil.h>
+
+/****************************************************************************************************************************************************/
+/*                                                            USER DEFINED CONSTANTS                                                                */
+/****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                             MAIN CODE START                                                                      */
+/****************************************************************************************************************************************************/
+
+#ifndef MAIN_AVIKODAK_SITES_CODECHEF_SCHOOL_AMR15A_H_
+#define MAIN_AVIKODAK_SITES_CODECHEF_SCHOOL_AMR15A_H_
+
+//Tested
+void solveProblem() {
+	unsigned int size, evenCount;
+	long long int input;
+	scanf("%u", &size);
+	evenCount = 0;
+	for (unsigned int counter = 0; counter < size; counter++) {
+		scanf("%lld", &input);
+		if (!(input & 1)) {
+			evenCount++;
+		}
+	}
+	printf("%s\n", evenCount > (size - evenCount) ? "READY FOR BATTLE" : "NOT READY");
+}
+
+#endif /* MAIN_AVIKODAK_SITES_CODECHEF_SCHOOL_AMR15A_H_ */
---
+++ src/main/avikodak/sites/codechef/school/chn09.h
@@ -0,0 +1,103 @@
+/****************************************************************************************************************************************************
+ *  File Name                   : chn09.h
+ *  File Location               : /algos_v2/src/main/avikodak/sites/codechef/school/chn09.h
+ *  Created on                  : Apr 17, 2016 :: 1:03:27 PM
+ *  Author                      : avikodak
+ *  Testing Status              : Tested
+ *  URL                         : https://www.codechef.com/problems/CHN09
+ ****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                         NAMESPACE DECLARATION AND IMPORTS                                                        */
+/****************************************************************************************************************************************************/
+
+using namespace std;
+using namespace __gnu_cxx;
+
+/****************************************************************************************************************************************************/
+/*                                                                 INCLUDES                                                                         */
+/****************************************************************************************************************************************************/
+
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <ctime>
+#include <list>
+#include <map>
+#include <set>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <hash_map>
+#include <stack>
+#include <queue>
+#include <limits.h>
+#include <libv2/common/commonincludes.h>
+#include <libv2/constants/constants.h>
+#include <libv2/ds/commonds.h>
+#include <libv2/ds/graphds.h>
+#include <libv2/ds/linkedlistds.h>
+#include <libv2/ds/mathds.h>
+#include <libv2/ds/treeds.h>
+#include <libv2/utils/abtreeutil.h>
+#include <libv2/utils/arrayutil.h>
+#include <libv2/utils/avltreeutil.h>
+#include <libv2/utils/bplustreeutil.h>
+#include <libv2/utils/bstutil.h>
+#include <libv2/utils/btreeutil.h>
+#include <libv2/utils/commonutil.h>
+#include <libv2/utils/dillutil.h>
+#include <libv2/utils/graphutil.h>
+#include <libv2/utils/ioutil.h>
+#include <libv2/utils/mathutil.h>
+#include <libv2/utils/redblacktreeutil.h>
+#include <libv2/utils/sillutil.h>
+#include <libv2/utils/treeutil.h>
+#include <libv2/utils/trieutil.h>
+#include <libv2/utils/twofourtreeutil.h>
+
+/****************************************************************************************************************************************************/
+/*                                                            USER DEFINED CONSTANTS                                                                */
+/****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                             MAIN CODE START                                                                      */
+/****************************************************************************************************************************************************/
+
+#ifndef MAIN_AVIKODAK_SITES_CODECHEF_SCHOOL_CHN09_H_
+#define MAIN_AVIKODAK_SITES_CODECHEF_SCHOOL_CHN09_H_
+
+//Tested
+void solveProblem() {
+	unsigned int testCases;
+	long long int aCount, bCount;
+	string userInput;
+	scanf("%u", &testCases);
+	while (testCases--) {
+		cin >> userInput;
+		aCount = 0;
+		bCount = 0;
+		for (unsigned int counter = 0; counter < userInput.size(); counter++) {
+			if (userInput[counter] == 'a') {
+				aCount++;
+			} else {
+				bCount++;
+			}
+		}
+		if (aCount == 0 || bCount == 0) {
+			printf("0\n");
+		} else {
+			printf("%lld\n", min(aCount, bCount));
+		}
+	}
+}
+
+#endif /* MAIN_AVIKODAK_SITES_CODECHEF_SCHOOL_CHN09_H_ */
