e8747a18df7f531850e6efdf473721c4986a53ee
Avinash Kodakandla <avikodak@gmail.com>
2016-04-20 00:24:02 +0530

    codechef

---
+++ src/main/avikodak/sites/codechef/school/fruits.h
@@ -0,0 +1,94 @@
+/****************************************************************************************************************************************************
+ *  File Name                   : fruits.h
+ *  File Location               : /algos_v2/src/main/avikodak/sites/codechef/school/fruits.h
+ *  Created on                  : Apr 20, 2016 :: 12:08:46 AM
+ *  Author                      : avikodak
+ *  Testing Status              : Tested
+ *  URL                         : https://www.codechef.com/problems/FRUITS
+ ****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                         NAMESPACE DECLARATION AND IMPORTS                                                        */
+/****************************************************************************************************************************************************/
+
+using namespace std;
+using namespace __gnu_cxx;
+
+/****************************************************************************************************************************************************/
+/*                                                                 INCLUDES                                                                         */
+/****************************************************************************************************************************************************/
+
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <ctime>
+#include <list>
+#include <map>
+#include <set>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <hash_map>
+#include <stack>
+#include <queue>
+#include <limits.h>
+#include <libv2/common/commonincludes.h>
+#include <libv2/constants/constants.h>
+#include <libv2/ds/commonds.h>
+#include <libv2/ds/graphds.h>
+#include <libv2/ds/linkedlistds.h>
+#include <libv2/ds/mathds.h>
+#include <libv2/ds/treeds.h>
+#include <libv2/utils/abtreeutil.h>
+#include <libv2/utils/arrayutil.h>
+#include <libv2/utils/avltreeutil.h>
+#include <libv2/utils/bplustreeutil.h>
+#include <libv2/utils/bstutil.h>
+#include <libv2/utils/btreeutil.h>
+#include <libv2/utils/commonutil.h>
+#include <libv2/utils/dillutil.h>
+#include <libv2/utils/graphutil.h>
+#include <libv2/utils/ioutil.h>
+#include <libv2/utils/mathutil.h>
+#include <libv2/utils/redblacktreeutil.h>
+#include <libv2/utils/sillutil.h>
+#include <libv2/utils/treeutil.h>
+#include <libv2/utils/trieutil.h>
+#include <libv2/utils/twofourtreeutil.h>
+
+/****************************************************************************************************************************************************/
+/*                                                            USER DEFINED CONSTANTS                                                                */
+/****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                             MAIN CODE START                                                                      */
+/****************************************************************************************************************************************************/
+
+#ifndef MAIN_AVIKODAK_SITES_CODECHEF_SCHOOL_FRUITS_H_
+#define MAIN_AVIKODAK_SITES_CODECHEF_SCHOOL_FRUITS_H_
+
+//Tested
+void solveProblem() {
+	unsigned int testCases;
+	long long int apples, oranges, goldCoins, difference;
+	scanf("%u", &testCases);
+	while (testCases--) {
+		scanf("%lld %lld %lld", &apples, &oranges, &goldCoins);
+		difference = abs(apples - oranges);
+		if (difference >= goldCoins) {
+			printf("%lld\n", difference - goldCoins);
+		} else {
+			printf("0\n");
+		}
+	}
+}
+
+#endif /* MAIN_AVIKODAK_SITES_CODECHEF_SCHOOL_FRUITS_H_ */
