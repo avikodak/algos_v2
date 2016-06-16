fbf1704d88eff16b6c43a4f59dd956806fdf2f79
Avinash Kodakandla <avikodak@gmail.com>
2016-06-17 00:26:09 +0530

    geeksforgeeks

---
+++ src/main/avikodak/sites/geeksforgeeks/practice/easy/distinctabsarrayelements.h
@@ -0,0 +1,120 @@
+/****************************************************************************************************************************************************
+ *  File Name                   : distinctabsarrayelements.h
+ *  File Location               : /algos_v2/src/main/avikodak/sites/geeksforgeeks/practice/easy/distinctabsarrayelements.h
+ *  Created on                  : Jun 16, 2016 :: 11:59:29 PM
+ *  Author                      : avikodak
+ *  Testing Status              : Tested
+ *  URL                         : http://www.practice.geeksforgeeks.org/problem-page.php?pid=903
+ ****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                         NAMESPACE DECLARATION AND IMPORTS                                                        */
+/****************************************************************************************************************************************************/
+
+using namespace std;
+using namespace __gnu_cxx;
+
+/****************************************************************************************************************************************************/
+/*                                                                 INCLUDES                                                                         */
+/****************************************************************************************************************************************************/
+
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <ctime>
+#include <list>
+#include <map>
+#include <set>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <hash_map>
+#include <stack>
+#include <queue>
+#include <limits.h>
+#include <libv2/common/commonincludes.h>
+#include <libv2/constants/constants.h>
+#include <libv2/ds/commonds.h>
+#include <libv2/ds/graphds.h>
+#include <libv2/ds/linkedlistds.h>
+#include <libv2/ds/mathds.h>
+#include <libv2/ds/treeds.h>
+#include <libv2/utils/abtreeutil.h>
+#include <libv2/utils/arrayutil.h>
+#include <libv2/utils/avltreeutil.h>
+#include <libv2/utils/bplustreeutil.h>
+#include <libv2/utils/bstutil.h>
+#include <libv2/utils/btreeutil.h>
+#include <libv2/utils/commonutil.h>
+#include <libv2/utils/dillutil.h>
+#include <libv2/utils/graphutil.h>
+#include <libv2/utils/ioutil.h>
+#include <libv2/utils/mathutil.h>
+#include <libv2/utils/redblacktreeutil.h>
+#include <libv2/utils/sillutil.h>
+#include <libv2/utils/treeutil.h>
+#include <libv2/utils/trieutil.h>
+#include <libv2/utils/twofourtreeutil.h>
+
+/****************************************************************************************************************************************************/
+/*                                                            USER DEFINED CONSTANTS                                                                */
+/****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                             MAIN CODE START                                                                      */
+/****************************************************************************************************************************************************/
+
+#ifndef MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_EASY_DISTINCTABSARRAYELEMENTS_H_
+#define MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_EASY_DISTINCTABSARRAYELEMENTS_H_
+
+//Tested
+void solveProblem() {
+    unsigned int testCases, size, frontCrawler, rearCrawler, uniqueCount;
+    long long int input, key;
+    vector<long long int> userInput;
+    scanf("%u", &testCases);
+    while (testCases--) {
+        scanf("%u", &size);
+        userInput.clear();
+        for (unsigned int counter = 0; counter < size; counter++) {
+            scanf("%lld", &input);
+            userInput.push_back(input);
+        }
+        frontCrawler = 0;
+        rearCrawler = size - 1;
+        uniqueCount = 0;
+        while (frontCrawler <= rearCrawler) {
+            if (abs(userInput[frontCrawler]) == abs(userInput[rearCrawler])) {
+                key = abs(userInput[frontCrawler]);
+                while (frontCrawler <= rearCrawler && abs(userInput[frontCrawler]) == key) {
+                    frontCrawler++;
+                }
+                while (frontCrawler <= rearCrawler && abs(userInput[rearCrawler]) == key) {
+                    rearCrawler--;
+                }
+            } else if (abs(userInput[frontCrawler]) > abs(userInput[rearCrawler])) {
+                key = abs(userInput[frontCrawler]);
+                while (frontCrawler <= rearCrawler && abs(userInput[frontCrawler]) == key) {
+                    frontCrawler++;
+                }
+            } else {
+                key = abs(userInput[rearCrawler]);
+                while (frontCrawler <= rearCrawler && abs(userInput[rearCrawler]) == key) {
+                    rearCrawler--;
+                }
+            }
+            uniqueCount++;
+        }
+        printf("%u\n", uniqueCount);
+    }
+}
+
+#endif /* MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_EASY_DISTINCTABSARRAYELEMENTS_H_ */
