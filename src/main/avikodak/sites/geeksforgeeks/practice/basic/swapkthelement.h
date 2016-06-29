11fa2b0f5312495cde5b9507b311b055206c9b8f
Avinash Kodakandla <avikodak@gmail.com>
2016-06-29 22:48:49 +0530

    geeksforgeeks

---
+++ src/main/avikodak/sites/geeksforgeeks/practice/basic/swapkthelement.h
@@ -0,0 +1,99 @@
+/****************************************************************************************************************************************************
+ *  File Name                   : swapkthelement.h
+ *  File Location               : /algos_v2/src/main/avikodak/sites/geeksforgeeks/practice/basic/swapkthelement.h
+ *  Created on                  : Jun 4, 2016 :: 1:42:48 PM
+ *  Author                      : avikodak
+ *  Testing Status              : Tested
+ *  URL                         : http://www.practice.geeksforgeeks.org/problem-page.php?pid=535
+ ****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                         NAMESPACE DECLARATION AND IMPORTS                                                        */
+/****************************************************************************************************************************************************/
+
+using namespace std;
+using namespace __gnu_cxx;
+
+/****************************************************************************************************************************************************/
+/*                                                                 INCLUDES                                                                         */
+/****************************************************************************************************************************************************/
+
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <ctime>
+#include <list>
+#include <map>
+#include <set>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <hash_map>
+#include <stack>
+#include <queue>
+#include <limits.h>
+#include <libv2/common/commonincludes.h>
+#include <libv2/constants/constants.h>
+#include <libv2/ds/commonds.h>
+#include <libv2/ds/graphds.h>
+#include <libv2/ds/linkedlistds.h>
+#include <libv2/ds/mathds.h>
+#include <libv2/ds/treeds.h>
+#include <libv2/utils/abtreeutil.h>
+#include <libv2/utils/arrayutil.h>
+#include <libv2/utils/avltreeutil.h>
+#include <libv2/utils/bplustreeutil.h>
+#include <libv2/utils/bstutil.h>
+#include <libv2/utils/btreeutil.h>
+#include <libv2/utils/commonutil.h>
+#include <libv2/utils/dillutil.h>
+#include <libv2/utils/graphutil.h>
+#include <libv2/utils/ioutil.h>
+#include <libv2/utils/mathutil.h>
+#include <libv2/utils/redblacktreeutil.h>
+#include <libv2/utils/sillutil.h>
+#include <libv2/utils/treeutil.h>
+#include <libv2/utils/trieutil.h>
+#include <libv2/utils/twofourtreeutil.h>
+
+/****************************************************************************************************************************************************/
+/*                                                            USER DEFINED CONSTANTS                                                                */
+/****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                             MAIN CODE START                                                                      */
+/****************************************************************************************************************************************************/
+
+#ifndef MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_BASIC_SWAPKTHELEMENT_H_
+#define MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_BASIC_SWAPKTHELEMENT_H_
+
+//Tested
+void solveProblem() {
+    unsigned int testCases, size, kthValue;
+    long long int input;
+    vector<long long int> userInput;
+    scanf("%u", &testCases);
+    while (testCases--) {
+        scanf("%u %u", &size, &kthValue);
+        userInput.clear();
+        for (unsigned int counter = 0; counter < size; counter++) {
+            scanf("%lld", &input);
+            userInput.push_back(input);
+        }
+        swap(userInput[kthValue - 1], userInput[userInput.size() - kthValue]);
+        for (unsigned int counter = 0; counter < size; counter++) {
+            printf("%lld ", userInput[counter]);
+        }
+        printf("\n");
+    }
+}
+
+#endif /* MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_BASIC_SWAPKTHELEMENT_H_ */
