254bd954ac44edd0b3e13b5e57129350b53df98f
Avinash Kodakandla <avikodak@gmail.com>
2016-07-12 22:17:13 +0530

    geeksforgeeks

---
+++ src/main/avikodak/sites/geeksforgeeks/practice/hard/largestnumberformed.h
@@ -0,0 +1,106 @@
+/****************************************************************************************************************************************************
+ *  File Name                   : largestnumberformed.h
+ *  File Location               : /algos_v2/src/main/avikodak/sites/geeksforgeeks/practice/hard/largestnumberformed.h
+ *  Created on                  : Jul 11, 2016 :: 11:09:28 PM
+ *  Author                      : avikodak
+ *  Testing Status              : Tested
+ *  URL                         : http://www.practice.geeksforgeeks.org/problem-page.php?pid=380
+ ****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                         NAMESPACE DECLARATION AND IMPORTS                                                        */
+/****************************************************************************************************************************************************/
+
+using namespace std;
+using namespace __gnu_cxx;
+
+/****************************************************************************************************************************************************/
+/*                                                                 INCLUDES                                                                         */
+/****************************************************************************************************************************************************/
+
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <ctime>
+#include <list>
+#include <map>
+#include <set>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <hash_map>
+#include <stack>
+#include <queue>
+#include <limits.h>
+#include <libv2/common/commonincludes.h>
+#include <libv2/constants/constants.h>
+#include <libv2/ds/commonds.h>
+#include <libv2/ds/graphds.h>
+#include <libv2/ds/linkedlistds.h>
+#include <libv2/ds/mathds.h>
+#include <libv2/ds/treeds.h>
+#include <libv2/utils/abtreeutil.h>
+#include <libv2/utils/arrayutil.h>
+#include <libv2/utils/avltreeutil.h>
+#include <libv2/utils/bplustreeutil.h>
+#include <libv2/utils/bstutil.h>
+#include <libv2/utils/btreeutil.h>
+#include <libv2/utils/commonutil.h>
+#include <libv2/utils/dillutil.h>
+#include <libv2/utils/graphutil.h>
+#include <libv2/utils/ioutil.h>
+#include <libv2/utils/mathutil.h>
+#include <libv2/utils/redblacktreeutil.h>
+#include <libv2/utils/sillutil.h>
+#include <libv2/utils/treeutil.h>
+#include <libv2/utils/trieutil.h>
+#include <libv2/utils/twofourtreeutil.h>
+
+/****************************************************************************************************************************************************/
+/*                                                            USER DEFINED CONSTANTS                                                                */
+/****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                             MAIN CODE START                                                                      */
+/****************************************************************************************************************************************************/
+
+#ifndef MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_HARD_LARGESTNUMBERFORMED_H_
+#define MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_HARD_LARGESTNUMBERFORMED_H_
+
+//Tested
+int sortFunc(string X, string Y) {
+    string XY = X.append(Y);
+    string YX = Y.append(X);
+    return XY.compare(YX) > 0 ? 1 : 0;
+}
+
+//Tested
+void solveProblem() {
+    unsigned int testCases, size;
+    string input;
+    vector<string> userInput;
+    scanf("%u", &testCases);
+    while (testCases--) {
+        scanf("%u", &size);
+        userInput.clear();
+        for (unsigned int counter = 0; counter < size; counter++) {
+            cin >> input;
+            userInput.push_back(input);
+        }
+        sort(userInput.begin(), userInput.end(), sortFunc);
+        for (unsigned int counter = 0; counter < userInput.size(); counter++) {
+            cout << userInput[counter];
+        }
+        cout << endl;
+    }
+}
+
+#endif /* MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_HARD_LARGESTNUMBERFORMED_H_ */
