438be02fb1ef5312d3cd2c76fd015cdae2562f52
Avinash Kodakandla <avikodak@gmail.com>
2016-07-16 20:12:02 +0530

    geeksforgeeks

---
+++ src/main/avikodak/sites/geeksforgeeks/practice/easy/specialarraytraversal.h
@@ -0,0 +1,103 @@
+/****************************************************************************************************************************************************
+ *  File Name                   : specialarraytraversal.h
+ *  File Location               : /algos_v2/src/main/avikodak/sites/geeksforgeeks/practice/easy/specialarraytraversal.h
+ *  Created on                  : Jun 23, 2016 :: 11:29:28 PM
+ *  Author                      : avikodak
+ *  Testing Status              : Tested
+ *  URL                         : http://www.practice.geeksforgeeks.org/problem-page.php?pid=973
+ ****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                         NAMESPACE DECLARATION AND IMPORTS                                                        */
+/****************************************************************************************************************************************************/
+
+using namespace std;
+using namespace __gnu_cxx;
+
+/****************************************************************************************************************************************************/
+/*                                                                 INCLUDES                                                                         */
+/****************************************************************************************************************************************************/
+
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <ctime>
+#include <list>
+#include <map>
+#include <set>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <hash_map>
+#include <stack>
+#include <queue>
+#include <limits.h>
+#include <libv2/common/commonincludes.h>
+#include <libv2/constants/constants.h>
+#include <libv2/ds/commonds.h>
+#include <libv2/ds/graphds.h>
+#include <libv2/ds/linkedlistds.h>
+#include <libv2/ds/mathds.h>
+#include <libv2/ds/treeds.h>
+#include <libv2/utils/abtreeutil.h>
+#include <libv2/utils/arrayutil.h>
+#include <libv2/utils/avltreeutil.h>
+#include <libv2/utils/bplustreeutil.h>
+#include <libv2/utils/bstutil.h>
+#include <libv2/utils/btreeutil.h>
+#include <libv2/utils/commonutil.h>
+#include <libv2/utils/dillutil.h>
+#include <libv2/utils/graphutil.h>
+#include <libv2/utils/ioutil.h>
+#include <libv2/utils/mathutil.h>
+#include <libv2/utils/redblacktreeutil.h>
+#include <libv2/utils/sillutil.h>
+#include <libv2/utils/treeutil.h>
+#include <libv2/utils/trieutil.h>
+#include <libv2/utils/twofourtreeutil.h>
+
+/****************************************************************************************************************************************************/
+/*                                                            USER DEFINED CONSTANTS                                                                */
+/****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                             MAIN CODE START                                                                      */
+/****************************************************************************************************************************************************/
+
+#ifndef MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_EASY_SPECIALARRAYTRAVERSAL_H_
+#define MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_EASY_SPECIALARRAYTRAVERSAL_H_
+
+//Tested
+void solveProblem() {
+    unsigned int testCases;
+    string userInput;
+    stack<char> auxSpace;
+    scanf("%u", &testCases);
+    while (testCases--) {
+        cin >> userInput;
+        for (unsigned int counter = 0; counter < userInput.size(); counter++) {
+            if (isalpha(userInput[counter])) {
+                auxSpace.push(userInput[counter]);
+            }
+        }
+        for (unsigned int counter = 0; counter < userInput.size(); counter++) {
+            if (isalpha(userInput[counter])) {
+                printf("%c", auxSpace.top());
+                auxSpace.pop();
+            } else {
+                printf("%c", userInput[counter]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+#endif /* MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_EASY_SPECIALARRAYTRAVERSAL_H_ */
