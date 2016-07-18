c2819aedf058fd3c1cf56345e9603a302016e0f3
Avinash Kodakandla <avikodak@gmail.com>
2016-07-18 18:46:57 +0530

    geeksforgeeks

---
+++ src/main/avikodak/sites/geeksforgeeks/practice/medium/tripletsuminarray.h
@@ -0,0 +1,114 @@
+/****************************************************************************************************************************************************
+ *  File Name                   : tripletsuminarray.h
+ *  File Location               : /algos_v2/src/main/avikodak/sites/geeksforgeeks/practice/medium/tripletsuminarray.h
+ *  Created on                  : Jul 17, 2016 :: 8:52:59 PM
+ *  Author                      : avikodak
+ *  Testing Status              : Tested
+ *  URL                         : http://www.practice.geeksforgeeks.org/problem-page.php?pid=732
+ ****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                         NAMESPACE DECLARATION AND IMPORTS                                                        */
+/****************************************************************************************************************************************************/
+
+using namespace std;
+using namespace __gnu_cxx;
+
+/****************************************************************************************************************************************************/
+/*                                                                 INCLUDES                                                                         */
+/****************************************************************************************************************************************************/
+
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <ctime>
+#include <list>
+#include <map>
+#include <set>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <hash_map>
+#include <stack>
+#include <queue>
+#include <limits.h>
+#include <libv2/common/commonincludes.h>
+#include <libv2/constants/constants.h>
+#include <libv2/ds/commonds.h>
+#include <libv2/ds/graphds.h>
+#include <libv2/ds/linkedlistds.h>
+#include <libv2/ds/mathds.h>
+#include <libv2/ds/treeds.h>
+#include <libv2/utils/abtreeutil.h>
+#include <libv2/utils/arrayutil.h>
+#include <libv2/utils/avltreeutil.h>
+#include <libv2/utils/bplustreeutil.h>
+#include <libv2/utils/bstutil.h>
+#include <libv2/utils/btreeutil.h>
+#include <libv2/utils/commonutil.h>
+#include <libv2/utils/dillutil.h>
+#include <libv2/utils/graphutil.h>
+#include <libv2/utils/ioutil.h>
+#include <libv2/utils/mathutil.h>
+#include <libv2/utils/redblacktreeutil.h>
+#include <libv2/utils/sillutil.h>
+#include <libv2/utils/treeutil.h>
+#include <libv2/utils/trieutil.h>
+#include <libv2/utils/twofourtreeutil.h>
+
+/****************************************************************************************************************************************************/
+/*                                                            USER DEFINED CONSTANTS                                                                */
+/****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                             MAIN CODE START                                                                      */
+/****************************************************************************************************************************************************/
+
+#ifndef MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_MEDIUM_TRIPLETSUMINARRAY_H_
+#define MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_MEDIUM_TRIPLETSUMINARRAY_H_
+
+//Tested
+void solveProblem() {
+    unsigned int testCases, size;
+    long long int input, requiredSum, thirdValue;
+    bool isSumFound;
+    vector<long long int> userInput;
+    map<long long int, bool> occurenceFlags;
+    scanf("%u", &testCases);
+    while (testCases--) {
+        scanf("%u %lld", &size, &requiredSum);
+        userInput.clear();
+        occurenceFlags.clear();
+        for (unsigned int counter = 0; counter < size; counter++) {
+            scanf("%lld", &input);
+            userInput.push_back(input);
+            occurenceFlags.insert(pair<long long int, bool>(input, true));
+        }
+        isSumFound = false;
+        for (unsigned int outerCrawler = 0; outerCrawler < userInput.size(); outerCrawler++) {
+            for (unsigned int innerCrawler = outerCrawler + 1; innerCrawler < size; innerCrawler++) {
+                thirdValue = requiredSum - userInput[outerCrawler] - userInput[innerCrawler];
+                if (occurenceFlags.find(thirdValue) != occurenceFlags.end()) {
+                    if (thirdValue != userInput[outerCrawler] && thirdValue != userInput[innerCrawler]) {
+                        isSumFound = true;
+                        break;
+                    }
+                }
+            }
+            if (isSumFound) {
+                break;
+            }
+        }
+        printf("%d\n", isSumFound ? 1 : 0);
+    }
+}
+
+#endif /* MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_MEDIUM_TRIPLETSUMINARRAY_H_ */
