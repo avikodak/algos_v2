4b4049a14492ca36e6ed675d56eb35bc79f92aab
Avinash Kodakandla <avikodak@gmail.com>
2016-08-10 23:02:43 +0530

    geeksforgeeks

---
+++ src/main/avikodak/sites/geeksforgeeks/practice/basic/missingrangeofnumbers.h
@@ -0,0 +1,113 @@
+/****************************************************************************************************************************************************
+ *  File Name                   : missingrangeofnumbers.h
+ *  File Location               : /algos_v2/src/main/avikodak/sites/geeksforgeeks/practice/basic/missingrangeofnumbers.h
+ *  Created on                  : Aug 8, 2016 :: 7:07:10 PM
+ *  Author                      : avikodak
+ *  Testing Status              : Tested
+ *  URL                         : http://www.practice.geeksforgeeks.org/problem-page.php?pid=1018
+ ****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                         NAMESPACE DECLARATION AND IMPORTS                                                        */
+/****************************************************************************************************************************************************/
+
+using namespace std;
+using namespace __gnu_cxx;
+
+/****************************************************************************************************************************************************/
+/*                                                                 INCLUDES                                                                         */
+/****************************************************************************************************************************************************/
+
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <ctime>
+#include <list>
+#include <map>
+#include <set>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <hash_map>
+#include <stack>
+#include <queue>
+#include <limits.h>
+#include <libv2/common/commonincludes.h>
+#include <libv2/constants/constants.h>
+#include <libv2/ds/commonds.h>
+#include <libv2/ds/graphds.h>
+#include <libv2/ds/linkedlistds.h>
+#include <libv2/ds/mathds.h>
+#include <libv2/ds/treeds.h>
+#include <libv2/utils/abtreeutil.h>
+#include <libv2/utils/arrayutil.h>
+#include <libv2/utils/avltreeutil.h>
+#include <libv2/utils/bplustreeutil.h>
+#include <libv2/utils/bstutil.h>
+#include <libv2/utils/btreeutil.h>
+#include <libv2/utils/commonutil.h>
+#include <libv2/utils/dillutil.h>
+#include <libv2/utils/graphutil.h>
+#include <libv2/utils/ioutil.h>
+#include <libv2/utils/mathutil.h>
+#include <libv2/utils/redblacktreeutil.h>
+#include <libv2/utils/sillutil.h>
+#include <libv2/utils/treeutil.h>
+#include <libv2/utils/trieutil.h>
+#include <libv2/utils/twofourtreeutil.h>
+
+/****************************************************************************************************************************************************/
+/*                                                            USER DEFINED CONSTANTS                                                                */
+/****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                             MAIN CODE START                                                                      */
+/****************************************************************************************************************************************************/
+
+#ifndef MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_BASIC_MISSINGRANGEOFNUMBERS_H_
+#define MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_BASIC_MISSINGRANGEOFNUMBERS_H_
+
+//Tested
+void solveProblem() {
+    unsigned int testCases, size;
+    int input, outerCrawler, innerCrawler;
+    scanf("%u", &testCases);
+    while (testCases--) {
+        scanf("%u", &size);
+        bool flags[1000] = { false };
+        for (unsigned int counter = 0; counter < size; counter++) {
+            scanf("%d", &input);
+            flags[input] = true;
+        }
+        printf("[ ");
+        outerCrawler = 0;
+        while (outerCrawler < 1000) {
+            innerCrawler = outerCrawler;
+            while (innerCrawler < 1000 && !flags[innerCrawler]) {
+                innerCrawler++;
+            }
+            if (innerCrawler > outerCrawler) {
+                if (innerCrawler - outerCrawler == 1) {
+                    printf("%d ", innerCrawler - 1);
+                } else {
+                    printf("%d-%d ", outerCrawler, innerCrawler - 1);
+                }
+            }
+            while (innerCrawler < 1000 && flags[innerCrawler]) {
+                innerCrawler++;
+            }
+            outerCrawler = innerCrawler;
+        }
+        printf("]\n");
+    }
+}
+
+#endif /* MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_BASIC_MISSINGRANGEOFNUMBERS_H_ */
