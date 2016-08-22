f0eb268a268fd1723ed2fde4a2235ecbbb772cc1
Avinash Kodakandla <avikodak@gmail.com>
2016-08-22 23:43:32 +0530

    geeksforgeeks

---
+++ src/main/avikodak/sites/geeksforgeeks/practice/easy/giftsgiftsgifts.h
@@ -0,0 +1,106 @@
+/****************************************************************************************************************************************************
+ *  File Name                   : giftsgiftsgifts.h
+ *  File Location               : /algos_v2/src/main/avikodak/sites/geeksforgeeks/practice/easy/giftsgiftsgifts.h
+ *  Created on                  : Aug 25, 2016 :: 10:28:31 PM
+ *  Author                      : avikodak
+ *  Testing Status              : Tested
+ *  URL                         : http://www.practice.geeksforgeeks.org/problem-page.php?pid=1100
+ ****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                         NAMESPACE DECLARATION AND IMPORTS                                                        */
+/****************************************************************************************************************************************************/
+
+using namespace std;
+using namespace __gnu_cxx;
+
+/****************************************************************************************************************************************************/
+/*                                                                 INCLUDES                                                                         */
+/****************************************************************************************************************************************************/
+
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <ctime>
+#include <list>
+#include <map>
+#include <set>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <hash_map>
+#include <stack>
+#include <queue>
+#include <limits.h>
+#include <libv2/common/commonincludes.h>
+#include <libv2/constants/constants.h>
+#include <libv2/ds/commonds.h>
+#include <libv2/ds/graphds.h>
+#include <libv2/ds/linkedlistds.h>
+#include <libv2/ds/mathds.h>
+#include <libv2/ds/treeds.h>
+#include <libv2/utils/abtreeutil.h>
+#include <libv2/utils/arrayutil.h>
+#include <libv2/utils/avltreeutil.h>
+#include <libv2/utils/bplustreeutil.h>
+#include <libv2/utils/bstutil.h>
+#include <libv2/utils/btreeutil.h>
+#include <libv2/utils/commonutil.h>
+#include <libv2/utils/dillutil.h>
+#include <libv2/utils/graphutil.h>
+#include <libv2/utils/ioutil.h>
+#include <libv2/utils/mathutil.h>
+#include <libv2/utils/redblacktreeutil.h>
+#include <libv2/utils/sillutil.h>
+#include <libv2/utils/treeutil.h>
+#include <libv2/utils/trieutil.h>
+#include <libv2/utils/twofourtreeutil.h>
+
+/****************************************************************************************************************************************************/
+/*                                                            USER DEFINED CONSTANTS                                                                */
+/****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                             MAIN CODE START                                                                      */
+/****************************************************************************************************************************************************/
+
+#ifndef MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_EASY_GIFTSGIFTSGIFTS_H_
+#define MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_EASY_GIFTSGIFTSGIFTS_H_
+
+//Tested
+void solveProblem() {
+    unsigned int testCases, size;
+    long long int input;
+    scanf("%u", &testCases);
+    while (testCases--) {
+        scanf("%u", &size);
+        vector<vector<long long int> > userInput(size);
+        vector<bool> giftsAvailability(size, true);
+        for (unsigned int outerCounter = 0; outerCounter < size; outerCounter++) {
+            for (unsigned int innerCounter = 0; innerCounter < size; innerCounter++) {
+                scanf("%lld", &input);
+                userInput[outerCounter].push_back(input);
+            }
+        }
+        for (unsigned int outerCounter = 0; outerCounter < size; outerCounter++) {
+            for (unsigned int innerCounter = 0; innerCounter < size; innerCounter++) {
+                if (giftsAvailability[userInput[outerCounter][innerCounter] - 1]) {
+                    printf("%lld ", userInput[outerCounter][innerCounter]);
+                    giftsAvailability[userInput[outerCounter][innerCounter] - 1] = false;
+                    break;
+                }
+            }
+        }
+        printf("\n");
+    }
+}
+
+#endif /* MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_EASY_GIFTSGIFTSGIFTS_H_ */
