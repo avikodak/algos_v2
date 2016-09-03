71e6765af6a76808a9373603ebab0f2a94552872
Avinash Kodakandla <avikodak@gmail.com>
2016-09-03 19:27:36 +0530

    geeksforgeeks

---
+++ src/main/avikodak/sites/geeksforgeeks/practice/school/maxval.h
@@ -0,0 +1,96 @@
+/****************************************************************************************************************************************************
+ *  File Name                   : maxval.h
+ *  File Location               : /algos_v2/src/main/avikodak/sites/geeksforgeeks/practice/school/maxval.h
+ *  Created on                  : Sep 5, 2016 :: 7:09:18 PM
+ *  Author                      : avikodak
+ *  Testing Status              : Tested
+ *  URL                         : http://www.practice.geeksforgeeks.org/problem-page.php?pid=1330
+ ****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                         NAMESPACE DECLARATION AND IMPORTS                                                        */
+/****************************************************************************************************************************************************/
+
+using namespace std;
+using namespace __gnu_cxx;
+
+/****************************************************************************************************************************************************/
+/*                                                                 INCLUDES                                                                         */
+/****************************************************************************************************************************************************/
+
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <ctime>
+#include <list>
+#include <map>
+#include <set>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <hash_map>
+#include <stack>
+#include <queue>
+#include <limits.h>
+#include <libv2/common/commonincludes.h>
+#include <libv2/constants/constants.h>
+#include <libv2/ds/commonds.h>
+#include <libv2/ds/graphds.h>
+#include <libv2/ds/linkedlistds.h>
+#include <libv2/ds/mathds.h>
+#include <libv2/ds/treeds.h>
+#include <libv2/utils/abtreeutil.h>
+#include <libv2/utils/arrayutil.h>
+#include <libv2/utils/avltreeutil.h>
+#include <libv2/utils/bplustreeutil.h>
+#include <libv2/utils/bstutil.h>
+#include <libv2/utils/btreeutil.h>
+#include <libv2/utils/commonutil.h>
+#include <libv2/utils/dillutil.h>
+#include <libv2/utils/graphutil.h>
+#include <libv2/utils/ioutil.h>
+#include <libv2/utils/mathutil.h>
+#include <libv2/utils/redblacktreeutil.h>
+#include <libv2/utils/sillutil.h>
+#include <libv2/utils/treeutil.h>
+#include <libv2/utils/trieutil.h>
+#include <libv2/utils/twofourtreeutil.h>
+
+/****************************************************************************************************************************************************/
+/*                                                            USER DEFINED CONSTANTS                                                                */
+/****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                             MAIN CODE START                                                                      */
+/****************************************************************************************************************************************************/
+
+#ifndef MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_SCHOOL_MAXVAL_H_
+#define MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_SCHOOL_MAXVAL_H_
+
+//Tested
+void solveProblem() {
+    unsigned int testCases, size;
+    long long int input, maxValue, minValue;
+    scanf("%u", &testCases);
+    while (testCases--) {
+        scanf("%u", &size);
+        maxValue = LLONG_MIN;
+        minValue = LLONG_MAX;
+        for (unsigned int counter = 0; counter < size; counter++) {
+            scanf("%lld", &input);
+            maxValue = max(maxValue, (input - counter));
+            minValue = min(minValue, (input - counter));
+        }
+        printf("%lld\n", maxValue - minValue);
+    }
+}
+
+#endif /* MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_SCHOOL_MAXVAL_H_ */
