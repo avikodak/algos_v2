f057580cb98c700d75038c6c72b39d38503651c1
Avinash Kodakandla <avikodak@gmail.com>
2016-09-13 19:23:38 +0530

    geeksforgeeks

---
+++ src/main/avikodak/sites/geeksforgeeks/practice/school/surfaceareavolumecuboid.h
@@ -0,0 +1,89 @@
+/****************************************************************************************************************************************************
+ *  File Name                   : surfaceareavolumecuboid.h
+ *  File Location               : /algos_v2/src/main/avikodak/sites/geeksforgeeks/practice/school/surfaceareavolumecuboid.h
+ *  Created on                  : Sep 6, 2016 :: 12:59:39 AM
+ *  Author                      : avikodak
+ *  Testing Status              : Tested
+ *  URL                         : http://www.practice.geeksforgeeks.org/problem-page.php?pid=298
+ ****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                         NAMESPACE DECLARATION AND IMPORTS                                                        */
+/****************************************************************************************************************************************************/
+
+using namespace std;
+using namespace __gnu_cxx;
+
+/****************************************************************************************************************************************************/
+/*                                                                 INCLUDES                                                                         */
+/****************************************************************************************************************************************************/
+
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <ctime>
+#include <list>
+#include <map>
+#include <set>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <hash_map>
+#include <stack>
+#include <queue>
+#include <limits.h>
+#include <libv2/common/commonincludes.h>
+#include <libv2/constants/constants.h>
+#include <libv2/ds/commonds.h>
+#include <libv2/ds/graphds.h>
+#include <libv2/ds/linkedlistds.h>
+#include <libv2/ds/mathds.h>
+#include <libv2/ds/treeds.h>
+#include <libv2/utils/abtreeutil.h>
+#include <libv2/utils/arrayutil.h>
+#include <libv2/utils/avltreeutil.h>
+#include <libv2/utils/bplustreeutil.h>
+#include <libv2/utils/bstutil.h>
+#include <libv2/utils/btreeutil.h>
+#include <libv2/utils/commonutil.h>
+#include <libv2/utils/dillutil.h>
+#include <libv2/utils/graphutil.h>
+#include <libv2/utils/ioutil.h>
+#include <libv2/utils/mathutil.h>
+#include <libv2/utils/redblacktreeutil.h>
+#include <libv2/utils/sillutil.h>
+#include <libv2/utils/treeutil.h>
+#include <libv2/utils/trieutil.h>
+#include <libv2/utils/twofourtreeutil.h>
+
+/****************************************************************************************************************************************************/
+/*                                                            USER DEFINED CONSTANTS                                                                */
+/****************************************************************************************************************************************************/
+
+/****************************************************************************************************************************************************/
+/*                                                             MAIN CODE START                                                                      */
+/****************************************************************************************************************************************************/
+
+#ifndef MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_SCHOOL_SURFACEAREAVOLUMECUBOID_H_
+#define MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_SCHOOL_SURFACEAREAVOLUMECUBOID_H_
+
+//Tested
+void solveProblem() {
+    unsigned int testCases;
+    long long int length, breadth, height;
+    scanf("%u", &testCases);
+    while (testCases--) {
+        scanf("%lld %lld %lld", &length, &breadth, &height);
+        printf("%lld %lld\n", 2 * (length * breadth + breadth * height + height * length), length * breadth * height);
+    }
+}
+
+#endif /* MAIN_AVIKODAK_SITES_GEEKSFORGEEKS_PRACTICE_SCHOOL_SURFACEAREAVOLUMECUBOID_H_ */
